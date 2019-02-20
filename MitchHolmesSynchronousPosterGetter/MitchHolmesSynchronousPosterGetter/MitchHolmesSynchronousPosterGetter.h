078fb9b64a1c90510e75387b18549b679c8262b6
= <hlmmit001@myuct.ac.za>
2019-02-20 13:37:04 +0200

    Initial upload of the framework

---
+++ MitchHolmesSynchronousPosterGetter/MitchHolmesSynchronousPosterGetter/MitchHolmesSynchronousPosterGetter.h
@@ -0,0 +1,19 @@
+//
+//  MitchHolmesSynchronousPosterGetter.h
+//  MitchHolmesSynchronousPosterGetter
+//
+//  Created by Mitchell Holmes on 2019/02/20.
+//  Copyright © 2019 Mitchell Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MitchHolmesSynchronousPosterGetter.
+FOUNDATION_EXPORT double MitchHolmesSynchronousPosterGetterVersionNumber;
+
+//! Project version string for MitchHolmesSynchronousPosterGetter.
+FOUNDATION_EXPORT const unsigned char MitchHolmesSynchronousPosterGetterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MitchHolmesSynchronousPosterGetter/PublicHeader.h>
+
+
